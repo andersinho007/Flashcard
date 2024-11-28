870a1c02b70f9278d559807899e482b72ee1cd5a
andersinho007 <bluerockbr007@gmail.com>
2024-11-28 01:32:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Eucação Fisica',
+    'É permitido tirar a bola do adversário no headboll?',
+    'É permitido a um jogador tomar a bola de um jogador adversário usando apenas uma mão e mantendo-a aberta. Não é permitido arrancar a bola da mão do adversário.'
+)
+
+criaCartao(
+    'Matematica',
+    'Raiz quadrada de 900? ',
+    '30'
+)
+
+criaCartao(
+    'Fisica',
+    'Oque Nikolas Tesla descobriu?',
+    'A bobina de Tesla, um dispositivo que emite belos fluxos de energia elétrica para tentar transmitir eletricidade sem fio'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o orgão mais delicado do corpo humano?',
+    'Pele'
+)
+
+criaCartao(
+    'Geografia',
+    'Capital da Belgica?',
+    'Bruxelas'
+)
+
+criaCartao(
+    'Historia',
+    'Quando foi o fim da segunda guerra mundial?',
+    '2 de setembro de 1945'
+)
